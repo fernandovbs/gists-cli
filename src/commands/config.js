778be8f0e78c3649637b052f8ee36c2dbb885bd8
Fernando Souza <fernandovbsouza@gmail.com>
2020-07-26 23:44:12 -0300

    Include config command

---
+++ src/commands/config.js
@@ -0,0 +1,42 @@
+const {Command, flags} = require('@oclif/command')
+const inquirer = require('inquirer')
+const fs = require('fs')
+const homedir = require('os').homedir()
+
+class ConfigCommand extends Command {
+  async run() {
+
+    inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'token',
+        message: `Please, go to https://github.com/settings/tokens/new and generate a Personal access token
+        with gists privilege and past it here:
+        `,
+      }      
+    ])
+    .then( ({token}) => {
+      fs.writeFile(`${homedir}/.gists-cli.json`,`{"token": "${token}"}`, (err) => {
+        if (err) throw err; 
+        this.log(`
+        Done!
+        Run gists find SOMETHING to find your gists`)
+      })      
+    })
+    .catch(error => {
+      if(error.isTtyError) {
+        this.error("couldn't be rendered in the current environment")
+      } else {
+        this.error("Something whent wrong")
+      }
+    })
+  }
+}
+
+ConfigCommand.description = `Set auth information
+...
+Generates .gists-cli.json to store the access token.
+`
+
+module.exports = ConfigCommand
