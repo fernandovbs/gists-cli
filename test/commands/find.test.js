ddf3ed394f83b4a4c8b1354f71af9a6fa90e9ba0
Fernando Souza <fernandovbsouza@gmail.com>
2020-07-26 23:44:48 -0300

    Include tests

---
+++ test/commands/find.test.js
@@ -0,0 +1,17 @@
+const {expect, test} = require('@oclif/test')
+
+describe('find', () => {
+  test
+  .stdout()
+  .command(['find'])
+  .it('runs hello', ctx => {
+    expect(ctx.stdout).to.contain('hello world')
+  })
+
+  test
+  .stdout()
+  .command(['find', '--name', 'jeff'])
+  .it('runs hello --name jeff', ctx => {
+    expect(ctx.stdout).to.contain('hello jeff')
+  })
+})
